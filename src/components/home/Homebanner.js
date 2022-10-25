5bb4bce37db37fb623cef1bddaae73558c2ddd9d
srsohan150626 <sohan.ice.pust@gmail.com>
2022-10-25 13:33:07 +0600

    useState hooks of functional component

--- src/components/home/Homebanner.js
+++ src/components/home/Homebanner.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Homebanner = (props) => {
+    const [color, setColor] = useState("Red");
+    const newColor = () => {
+      setColor("Green");
+    }
     return (
         <div>
             <h1>{props.title}</h1>
             <h3>{props.subtitle}</h3>
-            <h6>{props.json.name} {props.json.age}</h6>
+            <h4>{props.json.name} {props.json.age}</h4>
+            <h1>{color}</h1>
+            <button onClick={newColor}>Change Me</button>
         </div>
     );
 };
