8cb4eba41c31c68f67f7f7262f63ff815023b8fb
srsohan150626 <sohan.ice.pust@gmail.com>
2022-11-03 16:05:35 +0600

    learning update

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,50 @@
 import React from 'react';
 import Homebanner from "../components/functional/Homebanner";
 import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import cogoToast from "cogo-toast";
+import exportFromJSON from 'export-from-json'
+import * as htmlToImage from 'html-to-image';
 
 const Homepage = () => {
+    const InfoToast = () => {
+       let options = {
+           "position" : "top-right",
+           "hideAfter" : 3
+       }
+        cogoToast.info("Test Info", options);
+    }
+    const SuccessToast = () => {
+        cogoToast.success("Test Info");
+    }
+    const ExportCSV = () => {
+      const data = [
+          {
+              name: "Sohan",
+              city: "Dhaka"
+          },
+          {
+              name: "Saif",
+              city: "Kushtia"
+          }
+      ]
+        const fileName = 'Info';
+        const exportType =  exportFromJSON.types.xls;
+        exportFromJSON({ data, fileName, exportType });
+    }
+    const downloadImage = () => {
+        var node = document.getElementById('homepage');
+        htmlToImage.toPng(node)
+            .then(function (dataUrl) {
+                var img = new Image();
+                img.src = dataUrl;
+                document.body.appendChild(img);
+            })
+            .catch(function (error) {
+                console.error('oops, something went wrong!', error);
+            });
+    }
     return (
-        <div>
+        <div id="homepage">
             {/*<Homebanner title="React" subtitle="let's make some fun!" json={{name: "sohan", age: "26"}}/>*/}
             <Navbar bg="light" expand="lg">
                 <Container>
@@ -29,6 +69,10 @@ const Homepage = () => {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+            <button onClick={InfoToast} className="btn btn-outline-info">Info</button>
+            <button onClick={SuccessToast} className="btn btn-outline-success">Success</button>
+            <button onClick={ExportCSV} className="btn btn-outline-primary">Download CSV</button>
+            <button onClick={downloadImage} className="btn btn-outline-primary">Download Image</button>
         </div>
     );
 };
